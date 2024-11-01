34f86a0544323d98eff661794b770c185a253499
Jay Sachs <jaysachs@users.noreply.github.com>
2024-10-31 22:46:38 -0700

    update images

--- local/make_ziggurats.sh
+++ local/make_ziggurats.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 CMD=
-for C in 1 2 3 4 5 6 7 8 9
+for C in 10pts xturn hand7 3nobles 3farmers fields citypts land river
 do
-  CMD+=" \( canvas:none -stroke none -fill \#BBBBBB -draw 'rectangle 0,0 99,149' -stroke black -strokewidth 2 -fill black -draw 'text 0,0 "Zig_${C}"' \) "
+  CMD+=" \( canvas:none -stroke none -fill \#D6C6B5 -draw 'rectangle 0,0 99,149' -stroke black -strokewidth 2 -fill black -draw 'text 0,0 \""${C}"\"' \) "
 done
 
-CMD+=" \( canvas:none -stroke none -fill \#DDDDDD -draw 'rectangle 0,0 99,149' \) "
+CMD+=" \( canvas:none -stroke none -fill \#867665 -draw 'rectangle 0,0 99,149' \) "
 
-echo magick -size 100x150 -pointsize 24 -gravity center ${CMD} +append ziggurats.png
+echo magick -size 100x150 -pointsize 24 -gravity center ${CMD} +append ziggurats.png | bash -s
