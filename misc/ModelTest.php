b37c87a556b7cdd5349b92459e364c949e494fe9
Jay Sachs <jaysachs@users.noreply.github.com>
2024-11-18 10:00:18 -0800

    Make more use of fromTestMap in ModelTest

--- misc/ModelTest.php
+++ misc/ModelTest.php
@@ -20,7 +20,12 @@ class TestStore extends PersistentStore {
     private array $player_infos = [];
     private Components $components;
 
-    public function __construct() {
+    public static function fromMap(string $map): TestStore {
+        return new TestStore(Board::fromTestMap($map));
+    }
+
+    public function __construct(Board $board) {
+        $this->board = $board;
         PersistentStore::__construct(null);
         for ($i = 1; $i <= 3; $i++) {
             $this->player_infos[$i] = new PlayerInfo($i, "", 0, 0, 0, 5, 25);
@@ -41,33 +46,83 @@ class TestStore extends PersistentStore {
     public function retrieveComponents(): Components {
         return $this->components;
     }
+
+    public function hex(int $r, int $c) {
+        return $this->board->hexAt($r, $c);
+    }
 }
 
 final class ModelTest extends TestCase
 {
-    public function testCitiesRequiringScoring(): void
+    public function testCitiesRequiringScoringNoPieces(): void
     {
-        $board = Board::forPlayerCount(2, false);
-        $ps = new TestStore();
-        $ps->insertBoard($board);
+        $ps = new TestStore(Board::forPlayerCount(2));
         $model = new Model($ps, 0);
 
         $this->assertEquals([], $model->hexesRequiringScoring());
-
-        $board->hexAt(4, 0)->playPiece(Piece::PRIEST, 1);
-        $board->hexAt(5, 1)->playPiece(Piece::FARMER, 2);
-        $board->hexAt(7, 1)->playPiece(Piece::PRIEST, 3);
-        $this->assertEquals([], $model->hexesRequiringScoring());
-
-        $board->hexAt(8, 0)->playPiece(Piece::PRIEST, 3);
-        $this->assertEquals([$board->hexAt(6, 0)], $model->hexesRequiringScoring());
     }
+    const MAP1 = <<<'END'
+XXX   XXX  XXX
+   ---   XXX
+---   ===   ---
+   ZZZ   C.P
+p-1   ===   ---
+   f-2   ---
+C.M   ---
+   p-3
+---
+END;
+
+    public function testCitiesRequiringScoringNotSurrounded(): void {
+        $ps = TestStore::fromMap(ModelTest::MAP1);
+        $model = new Model($ps, 0);
+        $this->assertEquals([], $model->hexesRequiringScoring());
+    }
+
+    const MAP2 = <<<'END'
+XXX   XXX  XXX
+   ---   XXX
+---   ===   ---
+   ZZZ   C.P
+p-1   ===   ---
+   f-2   ---
+C.M   ---
+   p-3
+s-3
+END;
+
+    public function testCitiesRequiringScoringOneSurrounded(): void {
+        $ps = TestStore::fromMap(ModelTest::MAP2);
+        $model = new Model($ps, 0);
+        $this->assertEquals([$ps->hex(6, 0)],
+                            $model->hexesRequiringScoring());
+    }
+
+    const MAP3 = <<<'END'
+XXX   XXX  XXX
+   C.S   XXX
+---   h-1   f-2
+   ZZZ   C.P
+p-1   h-3   s-2
+   f-2   m-1
+C.M   ---
+   p-3
+s-3
+END;
+
+    public function testCitiesRequiringScoringMultipleSurrounded(): void {
+        $ps = TestStore::fromMap(ModelTest::MAP3);
+        $model = new Model($ps, 0);
+        $this->assertEqualsCanonicalizing(
+            [$ps->hex(6, 0), $ps->hex(3,3)],
+            $model->hexesRequiringScoring());
+    }
+
 
     public function testZigguratsRequiringScoring(): void
     {
         $board = Board::forPlayerCount(2);
-        $ps = new TestStore();
-        $ps->insertBoard($board);
+        $ps = new TestStore($board);
         $model = new Model($ps, 0);
 
         $this->assertEquals([], $model->hexesRequiringScoring());
