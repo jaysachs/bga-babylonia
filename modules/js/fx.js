6f1176ddf143cd799fd6db983fab0aa37cdc28e1
Jay Sachs <jaysachs@users.noreply.github.com>
2024-11-23 23:02:03 -0800

    Use a containing span as well to avoid size issues when inside the
    BGA DOM.

--- modules/js/fx.js
+++ modules/js/fx.js
@@ -17,46 +17,54 @@ define([
     return {
         spinGrowText : function(params = defParms) {
             let p = Object.assign(Object.assign({}, defParams), params);
-            console.log("spinGrowText", p);
             let id = `spinGrowFx-${lastId++}`;
-            let node = dojo.place(`<span id="${id}">${p.text}</span>`, p.parent);
+            // We use a container node to hold the final size.
+            //   something in BGA's CSS and structure gets in the way
+            //   and shrinks the node down to its minimum.
+            let outer = dojo.place(`<span id="${id}">${p.text}</span>`, p.parent);
+            outer.style.color = "transparent";
+            outer.style.position = "absolute";
+            outer.style.fontSize = p.fontSize + "pt";
+            outer.style.display = "inline-block";
+            outer.style.justifyContent = "center";
+            outer.style.alignItems = "center";
+            outer.style.display = "flex";
+            // probably should allow a class to be passed in and used for these two
+            outer.style.fontFamily = "Helvetica";
+            outer.style.fontStyle = "bold";
 
-            node.style.color = "transparent";
-            node.style.position = "absolute";
-            node.style.fontSize = "128pt";
-            node.style.fontFamily = "Helvetica";
-            node.style.fontStyle = "bold";
-            node.style.display = "inline-block";
-            node.style.justifyContent = "center";
-            node.style.alignItems = "center";
+            // get the ultimate dimensions of the container span
+            let nrect = outer.getBoundingClientRect();
+            outer.style.width = nrect.width;
+            outer.style.height = nrect.height;
 
-            // get the ultimate dimensions of the span
-            let nrect = node.getBoundingClientRect();
-            console.log(nrect);
-            node.style.width = nrect.width;
-            node.style.height = nrect.height;
-            // make text not viewable
-            node.style.fontSize = "0pt";
-            node.style.display = "flex";
-            node.style.zIndex = "100";
-            node.style.textShadow = "-2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000";
-
-
-            // center the node on the center of the appropriate node
+            // center the container on the center of the appropriate node
             let centerNode = dom.byId(p.centeredOn || p.parent);
             let prect = dom.byId(p.parent).getBoundingClientRect();
             let crect = centerNode.getBoundingClientRect();
-            console.log(prect);
-            console.log(crect);
-            console.log(dom.byId('bbl_board').getBoundingClientRect());
             let left = (crect.left + crect.width/2 - nrect.width/2 - prect.left);
             let top = (crect.top + crect.height/2 - nrect.height/2 - prect.top);
+            outer.style.left = left + "px";
+            outer.style.top = top + "px";
 
-            node.style.left = left + "px";
-            node.style.top = top + "px";
+            // now create the node we're animating
+            let node = dojo.place(`<span>${p.text}</span>`, id);
+            node.style.position = "absolute";
+            node.style.display = "inline-block";
+            node.style.justifyContent = "center";
+            node.style.alignItems = "center";
+            node.style.display = "flex";
+            // text not viewable
+            node.style.fontSize = "0pt";
+            // keep on top
+            node.style.zIndex = "100";
+
+            // this maybe ought to be a parameter, or part of the incoming class.
+            node.style.textShadow = "-2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000";
 
             return fx.chain([
                 dojo.fx.combine([
+                    // the "grow"
                     new dojo.Animation({
                         duration: p.duration,
                         curve: [0, p.fontSize],
@@ -64,8 +72,8 @@ define([
                         onAnimate: function(v) {
                             node.style.fontSize = `${v}pt`;
                         },
-                    })
-                    ,
+                    }),
+                    // the "spin"
                     new dojo.Animation({
                         duration: p.duration,
                         curve: [0, p.spinCount * 360],
@@ -74,11 +82,12 @@ define([
                         }
                     })
                 ]),
+                // and the fade out
                 fx.fadeOut({
                     node: node,
                     duration: p.fadeTime,
                     onEnd: () => {
-                        dojo.destroy(node);
+                        dojo.destroy(outer);
                     }
                 }),
             ]);
