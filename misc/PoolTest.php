7448e1156b59a601ce2e1f7bf5c319c4112b57b6
Jay Sachs <jaysachs@users.noreply.github.com>
2024-12-09 19:05:57 -0800

    Remove redundant test.

--- misc/PoolTest.php
+++ misc/PoolTest.php
@@ -14,16 +14,6 @@ final class PoolTest extends TestCase
     public function testTakeAll(): void
     {
         $pool = Pool::new();
-        $p = [];
-        while ($pool->size() > 0) {
-            $p[] = $pool->take(false);
-        }
-        $this->assertEquals(0, $pool->size());
-    }
-
-    public function testTakeAllRandom(): void
-    {
-        $pool = Pool::new();
         $all = $pool->pieces();
         $p = [];
         while ($pool->size() > 0) {
