891877cee1a051dc6fd52e1b0f28dd7ca632b681
Jay Sachs <jaysachs@users.noreply.github.com>
2025-01-18 20:52:13 -0800

    fix test

--- misc/ScorerTest.php
+++ misc/ScorerTest.php
@@ -16,9 +16,9 @@ final class ScorerTest extends TestCase
 {
     private function playerInfos(): array {
         return [
-            1 => new PlayerInfo(1, 1, 0, 3, 0, 0),
-            2 => new PlayerInfo(2, 2, 0, 0, 0, 0),
-            3 => new PlayerInfo(3, 3, 0, 2, 0, 0)
+            1 => new PlayerInfo(1, 0, 3, 0, 0),
+            2 => new PlayerInfo(2, 0, 0, 0, 0),
+            3 => new PlayerInfo(3, 0, 2, 0, 0)
         ];
     }
 
