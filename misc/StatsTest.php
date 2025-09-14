55830581b1b98406a7c462019dbdfce54281d892
Jay Sachs <jaysachs@users.noreply.github.com>
2025-09-14 07:57:37 -0400

    Use generated TestStatsImpl for StatsTest.

--- misc/StatsTest.php
+++ misc/StatsTest.php
@@ -11,55 +11,19 @@ use Bga\Games\babylonia\{
     StatsImpl
 };
 
-class SImpl implements StatsImpl {
-    public $vals = [];
-    public function initStat(string $cat, string $name, mixed $value, ?int $player_id = 0): void {
-        if ($player_id === null) {
-            $this->vals[$name] = $value;
-        } else {
-            if (! isset($this->vals[$name])) {
-                $this->vals[$name] = [];
-            }
-            $this->vals[$name][$player_id] = $value;
-        }
-    }
-
-    public function incStat(mixed $delta, string $name, ?int $player_id = 0): void {
-        if ($player_id === null) {
-            $this->vals[$name] += $delta;
-        } else {
-            $this->vals[$name][$player_id] += $delta;
-        }
-    }
-
-    public function setStat($value, $name, ?int $player_id = 0): void {
-        if ($player_id === null) {
-            $this->vals[$name] = $value;
-        } else {
-            $this->vals[$name][$player_id] = $value;
-        }
-    }
-
-    public function getStat($name, ?int $player_id = 0): mixed {
-        if ($player_id === null) {
-            return $this->vals[$name];
-        }
-        return $this->vals[$name][$player_id];
-    }
-}
 
 final class StatsTest extends TestCase
 {
     public function testInit(): void
     {
-        $stats = new Stats(new SImpl());
+        $stats = new Stats(new TestStatsImpl());
         $stats->initAll([1,2,3]);
         $this->assertTrue(true);
     }
 
     public function testSetGet(): void
     {
-        $stats = new Stats(new SImpl());
+        $stats = new Stats(new TestStatsImpl());
         $stats->initAll([0, 2]);
         $stats->PLAYER_NUMBER_TURNS->set(2, 4);
 
