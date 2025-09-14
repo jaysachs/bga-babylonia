0d6a2a67ccff0c8f1ef8658e55aaa21575dc1e89
Jay Sachs <jaysachs@users.noreply.github.com>
2025-09-14 07:58:09 -0400

    Add another PHPUnit method to _local_ide_helper.php

--- _local_ide_helper.php
+++ _local_ide_helper.php
@@ -4,6 +4,7 @@ namespace PHPUnit\Framework {
         protected function assertNull(mixed $a) { }
         protected function assertTrue(bool $t) { }
         protected function assertFalse(bool $t) { }
+        protected function assertSame(mixed $a, mixed $b) { }
         protected function assertEquals(mixed $a, mixed $b) { }
         protected function assertEqualsCanonicalizing(mixed $a, mixed $b) { }
     }
