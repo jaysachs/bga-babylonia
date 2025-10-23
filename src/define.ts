10a4e2117a220781c21e509c17be52199ff0d8c2
Jay Sachs <jaysachs@users.noreply.github.com>
2025-10-23 13:48:21 -0400

    Remove another dojo reference

--- src/define.ts
+++ src/define.ts
@@ -1,11 +1,10 @@
 
 define([
-  'dojo',
   'dojo/_base/declare',
   'ebg/core/gamegui',
   'ebg/counter',
   getLibUrl('bga-animations', '1.x'),
-], function (dojo, declare, gamegui, counter, BgaAnimations) {
+], function (declare, gamegui, counter, BgaAnimations) {
   (window as any).BgaAnimations = BgaAnimations;
   declare('bgagame.babylonia', ebg.core.gamegui, new BabyloniaGame());
 });
