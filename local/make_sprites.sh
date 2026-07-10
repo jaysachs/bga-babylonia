aae5479501f3ff5e1dfac4af11d6b43ec1d38ff6
Jay Sachs <jaysachs@users.noreply.github.com>
2026-07-10 17:45:58 -0400

    Further sprite work.

--- local/make_sprites.sh
+++ local/make_sprites.sh
@@ -6,7 +6,7 @@ HEX4="8,86 48,8 148,8 190,86 148,164 48,164"
 INNERHEX="0,43 12,22 88,22 99,43 88,66 12,66"
 SMALLHEX="21,68 29,54 71,54 79,68 71,82 29,82"
 
-COLORS=("${(@f)$(strip-json-comments gameinfos.jsonc | jq '.["player_colors"].[]')}")
+COLORS=("${(@f)$(strip-json-comments ../../../gameinfos.jsonc | jq '.["player_colors"].[]')}" bcb7a8)
 
 # tile back
 # addCanvas "-stroke none -fill \#D6C6B5 -draw 'polygon ${HEX}'"
@@ -28,8 +28,8 @@ I=1
 for COLOR in ${COLORS[*]}
 do
     C=${COLOR//\"/}
-    magick -size ${SIZE} -gravity center canvas:none -stroke black -fill \#${C} -draw 'circle 100,86 100,168' +append hidden-p-${I}.png
+    magick -size ${SIZE} -gravity center canvas:none -stroke black -fill \#${C} -draw 'circle 100,86 100,168' +append P-${I}-hidden.png
     # the player board "pool" icon
-    magick -size ${SIZE} -gravity center canvas:none -stroke black -fill \#${C} -draw 'circle 62,60 102,86' -stroke black -fill \#${C} -draw 'circle 100,120 140,146' -stroke black -fill \#${C} -draw 'circle 138,60 178,86' -transparent \#000001 +append pool-p-${I}.png
+    magick -size ${SIZE} -gravity center canvas:none -stroke black -fill \#${C} -draw 'circle 62,60 102,86' -stroke black -fill \#${C} -draw 'circle 100,120 140,146' -stroke black -fill \#${C} -draw 'circle 138,60 178,86' -transparent \#000001 +append P-${I}-pool.png
     I=$((I+1))
 done
