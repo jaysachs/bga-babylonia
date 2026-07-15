fa5205d3a5824fb63ab82ad8dc374cd9d3179818
Jay Sachs <jaysachs@users.noreply.github.com>
2026-07-14 23:25:02 -0400

    Switch images to invidual files while artwork is in flux.

--- local/make_zcards.sh
+++ local/make_zcards.sh
@@ -2,16 +2,10 @@
 
 # copied from make_sprites.sh
 ZIG="66,61 70,45 74,45 78,29 82,29 84,21 116,21 118,29 122,29 126,45 130,45 134,61"
-# ZIG="16,61 20,45 24,45 28,29 32,29 34,21 66,21 68,29 72,29 76,45 80,45 84,61"
 
-CMD=
 NUM=1
 for C in '+10pts' '+ turn' hand7 3nobles 3farmers fields citypts land river
 do
-    CMD+=" \( canvas:none -stroke black -fill \#D6C6B5 -draw 'rectangle 0,0,183,227' -fill \#924018 -draw 'polygon ${ZIG}' -stroke black -strokewidth 2 -fill black -gravity center -pointsize 42 -draw 'text 0,0 \""${C}"\"' -gravity SouthWest -pointsize 24 -strokewidth 1 -draw 'text 8,5 \""$((NUM++))"\"' \) "
+    magick -size 184x228 -pointsize 24 -gravity center canvas:none -stroke black -fill \#D6C6B5 -draw 'rectangle 0,0,183,227' -fill \#924018 -draw "polygon ${ZIG}" -stroke black -strokewidth 2 -fill black -gravity center -pointsize 42 -draw "text 0,0 '${C}'" -gravity SouthWest -pointsize 24 -strokewidth 1 -draw "text 8,5 '${NUM++}'" +append ZigCard-$((NUM++)).png
 
 done
-
-# CMD+=" \( canvas:none -stroke none -fill \#867665 -draw 'rectangle 0,0 183,227' \) "
-
-echo magick -size 184x228 -pointsize 24 -gravity center ${CMD} +append img/zcards.png | bash -s
