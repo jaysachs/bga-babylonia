437480551da118e964dec60b2cad3efc1006c7d7
Jay Sachs <jay@covariant.org>
2026-09-08 11:55:36 -0400

    Better phpstan typings.

--- modules/php/States/ScoreHex.php
+++ modules/php/States/ScoreHex.php
@@ -54,7 +54,7 @@ class ScoreHex extends AbstractState
         return $rc;
     }
 
-    /** @return array{row:int,col:int,city:string} */
+    /** @return array{hex:string,current_scoring_hex:int,city:string} */
     public function getArgs(): array {
         $rc = $this->scoringHex();
         $model = $this->createModel(0);
