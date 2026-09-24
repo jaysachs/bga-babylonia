4ea5cefe104179305a2936d9954d4af3461f9fe8
Jay Sachs <jay@covariant.org>
2026-09-23 21:04:42 -0400

    console.log => console.debug

--- src/babylonia.ts
+++ src/babylonia.ts
@@ -97,7 +97,7 @@ export class Game extends BaseGame<BblPlayer, BGamedatas> {
     }
 
     private renderHexForLog(hex: any, args: any): HTMLElement {
-        console.log(hex, args.rc, args.captured_city_count);
+        console.debug(hex, args.rc, args.captured_city_count);
         if (typeof hex != 'number') { hex = args.rc ?? args.captured_city_count; }
         const span = Html.span({ text: Hex.format(hex), classes: 'bbl_formattedhex' });
         span.setAttribute('bbl_hex', String(hex));
